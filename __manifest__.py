c3765dcda1a80f2c4ef337e4445c23633f03f3c1
junex10 <joaealejunior@gmail.com>
2021-03-24 18:54:17 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name" : "SuperCauchos",
+    "description" : "Supercauchos modulo",
+    "depends": ['sale_management'],
+    "data" : [
+        #"security/ir.model.access.csv",
+        "views/listado_precios_ventas.xml",
+        "views/security.xml",
+        "views/listado_precios_transito.xml"
+    ]
+}
---
+++ models/models.py
@@ -0,0 +1,33 @@
+from odoo import fields, models, api
+
+class SuperCauchosVentas(models.Model):
+    _name = 'ventas.disp'
+    _description = 'Ventas lista de productos'
+
+    cant_disponible = fields.Integer('cant_disp')
+    medidas = fields.Char('medidas')
+    modelo = fields.Char('modelo')
+    lonas = fields.Char('lonas')
+    marca = fields.Char('marca')
+
+    precio_dolar_uno = fields.Float('precio_dolar_uno')
+    precio_bolivar_uno = fields.Float('precio_bolivar_uno')
+
+    precio_dolar_dos = fields.Float('precio_dolar_dos')
+    precio_bolivar_dos = fields.Float('precio_bolivar_dos')
+
+    precio_dolar_tres = fields.Float('precio_dolar_tres')
+    precio_bolivar_tres = fields.Float('precio_bolivar_tres')
+
+    estado = fields.Selection(selection=[('activo', 'Activo'), ('inactivo', 'Inactivo'), ('transito', 'En transito')])
+
+    imagen = fields.Binary()
+
+    titulo_producto = fields.Char('Titulo del reporte')
+
+class ProductosDisponibles(models.Model):
+
+    _inherit = 'ventas.disp'
+
+    def _ProductosDisponibles():
+        
