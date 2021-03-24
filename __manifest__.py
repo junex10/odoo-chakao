0726569289b26acccbbb4b6f86d031a43dcec934
junex10 <joaealejunior@gmail.com>
2021-03-24 19:47:23 -0400

    a

--- __manifest__.py
+++ __manifest__.py
@@ -4,8 +4,8 @@
     "depends": ['sale_management'],
     "data" : [
         #"security/ir.model.access.csv",
+        "views/listado_precios_transito.xml",
         "views/listado_precios_ventas.xml",
         "views/security.xml",
-        "views/listado_precios_transito.xml"
     ]
 }
--- models/models.py
+++ models/models.py
@@ -19,15 +19,44 @@ class SuperCauchosVentas(models.Model):
     precio_dolar_tres = fields.Float('precio_dolar_tres')
     precio_bolivar_tres = fields.Float('precio_bolivar_tres')
 
-    estado = fields.Selection(selection=[('activo', 'Activo'), ('inactivo', 'Inactivo'), ('transito', 'En transito')])
+    estado = fields.Selection(selection=[('activo', 'Activo'), ('inactivo', 'Inactivo'), ('transito', 'En transito')], default="activo")
 
     imagen = fields.Binary()
 
     titulo_producto = fields.Char('Titulo del reporte')
 
-class ProductosDisponibles(models.Model):
+    def _disponiblesProductos(self):
+        busqueda = self.env['ventas.disp'].search([
+            ('estado', '!=', 'transito')
+        ])
+        self.datos = busqueda
 
-    _inherit = 'ventas.disp'
 
-    def _ProductosDisponibles():
-        
+class ProductosTransito(models.Model):
+    _name ='ventas.transito'
+    _description = 'Ventas en lista de transito'
+
+    cant_disponible = fields.Integer('cant_disp')
+    medidas = fields.Char('medidas')
+    modelo = fields.Char('modelo')
+    lonas = fields.Char('lonas')
+    marca = fields.Char('marca')
+
+    precio_dolar_uno = fields.Float('precio_dolar_uno')
+    precio_bolivar_uno = fields.Float('precio_bolivar_uno')
+
+    precio_dolar_dos = fields.Float('precio_dolar_dos')
+    precio_bolivar_dos = fields.Float('precio_bolivar_dos')
+
+    precio_dolar_tres = fields.Float('precio_dolar_tres')
+    precio_bolivar_tres = fields.Float('precio_bolivar_tres')
+
+    imagen = fields.Binary()
+
+    titulo_producto = fields.Char('Titulo del reporte')
+
+    def _transitoProductos(self):
+        busqueda = self.env['ventas.transito'].search([
+            ('estado', '=', 'transito')
+        ])
+        self.datos = busqueda
