53865b5b57948d1c9565dfc51647595026717f6c
junex10 <joaealejunior@gmail.com>
2021-03-24 20:24:03 -0400

    a

--- models/transito.py
+++ models/transito.py
@@ -1,29 +1,9 @@
 from odoo import fields, models, api
 class ProductosTransito(models.Model):
-    _name ='ventas.transito'
-    _description = 'Ventas en lista de transito'
+    _inherit = 'ventas.disp'
 
-    cant_disponible = fields.Integer('cant_disp')
-    medidas = fields.Char('medidas')
-    modelo = fields.Char('modelo')
-    lonas = fields.Char('lonas')
-    marca = fields.Char('marca')
-
-    precio_dolar_uno = fields.Float('precio_dolar_uno')
-    precio_bolivar_uno = fields.Float('precio_bolivar_uno')
-
-    precio_dolar_dos = fields.Float('precio_dolar_dos')
-    precio_bolivar_dos = fields.Float('precio_bolivar_dos')
-
-    precio_dolar_tres = fields.Float('precio_dolar_tres')
-    precio_bolivar_tres = fields.Float('precio_bolivar_tres')
-
-    imagen = fields.Binary()
-
-    titulo_producto = fields.Char('Titulo del reporte')
-
-    def _transitoProductos(self):
-        busqueda = self.env['ventas.transito'].search([
+    def _disponiblesProductos(self):
+        busqueda = self.env['ventas.disp'].search([
             ('estado', '=', 'transito')
         ])
         self.datos = busqueda
